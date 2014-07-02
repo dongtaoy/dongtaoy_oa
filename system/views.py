4d48dad25c786a011e5e8a8a95b01fa0c1ee9249
Dongtao Yu <dongt.yu@gmail.com>
2014-07-03 01:25:59 +0800

    init.d

--- system/views.py
+++ system/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.db import transaction
 from oa_model.models import OaGroup, OaGroupPermission, OaPermission
 from dongtaoy_oa.views import permission_tree
 from django_ajax.decorators import ajax
 from json import loads
+
 from django.http import HttpResponse
 
 
@@ -21,8 +23,20 @@ def permission_detail(request):
                                                           "group_permissions": group_permissions,
                                                           "groupid": request.GET.get('groupid')})
 
+
+# AJAX save permission
 @ajax
 def permission_save(request):
-    permissions = loads(request.POST.get('permissions'))
-    print permissions
-    return 1
+    new_permissions = loads(request.POST.get('permissions'))
+    print new_permissions
+    group = OaGroup.objects.get(id=request.POST.get('groupid'))
+    OaGroupPermission.objects.filter(group=group).delete()
+    with transaction.atomic():
+        for permission in new_permissions:
+            entry = OaGroupPermission(group=group, permission=OaPermission.objects.get(id=permission))
+            entry.save()
+    all_permissions = OaPermission.objects.all()
+    group_permissions = [x.permission for x in OaGroupPermission.objects.filter(group=group)]
+    return render(request, 'system/permission/mod.html', {"all_permissions": permission_tree(all_permissions),
+                                                          "group_permissions": group_permissions,
+                                                          "groupid": group.id})
