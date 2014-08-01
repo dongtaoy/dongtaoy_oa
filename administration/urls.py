7a31a17169331bbd2eaeac99b8f17f01e443562c
Dongtao Yu <dongt.yu@gmail.com>
2014-08-01 18:56:34 +1000

    init.d

--- administration/urls.py
+++ administration/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'dongtaoy_oa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    # assets url
+    # assets urls
     url(r'^asset/', include('administration.asset.urls')),
+
+    # assets category urls
+    url(r'^category/', include('administration.category.urls'))
 )
