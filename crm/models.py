1b4844175826f25296021d4eaadbb1828ccdbb58
dongtaoy <dongt.yu@gmail.com>
2014-08-02 12:48:05 +1000

    init.d

--- crm/models.py
+++ crm/models.py
@@ -1,15 +1,16 @@
+__author__ = 'kay'
 from django.db import models
 
 
-class Customer(models.Model):
+class Customers(models.Model):
     name = models.CharField(max_length=45)
     phone = models.IntegerField(max_length=15, blank=True)
-    address = models.CharField(max_length=100, blank=True, null=True)
-    email = models.CharField(max_length=50, blank=True)
-    type = models.ForeignKey('crm.CustomerType', blank=True, null=True, default=None)
+    address = models.CharField(max_length=100,blank=True, null=True)
+    email = models.CharField(max_length=50,blank=True)
+    fax = models.IntegerField(blank=True,null=True)
+    ctype = models.ForeignKey('crm.Ctype', blank=True, null=True, default=None)
 
-
-class CustomerType(models.Model):
+class Ctype(models.Model):
     name = models.CharField(max_length=20)
-    description = address = models.CharField(max_length=100, blank=True, null=True)
-    label = models.CharField(max_length=20, blank=True, null=True)
+    description = address = models.CharField(max_length=100,blank=True, null=True)
+    label = models.CharField(max_length=20,blank=True, null=True)
