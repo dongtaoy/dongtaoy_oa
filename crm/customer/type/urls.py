7aefee2984fb0ef1a8a25f15a80eff131b6e8d05
zhangjingyuan <barrel.titor.daru@gmail.com>
2014-08-02 14:00:20 +1000

    init.d

--- crm/customer/type/urls.py
+++ crm/customer/type/urls.py
@@ -6,4 +6,16 @@ urlpatterns = patterns('',
     # url(r'^$', 'dongtaoy_oa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^$','dongtaoy_oa.crm.views.customer_index'),
+
+    #index
+    url(r'^$', 'crm.customer.type.views.type_index'),
+
+    # detail
+    url(r'^ajax/detail/', 'crm.customer.type.views.type_detail'),
+
+    # save
+    url(r'^ajax/save/', 'crm.customer.type.views.type_save'),
+
+    # delete
+    url(r'^ajax/delete/', 'crm.customer.type.views.type_delete')
 )
