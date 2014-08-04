9740960302cead224d5901b71e8cbcfedb66974d
Dongtao Yu <dongt.yu@gmail.com>
2014-08-04 13:30:16 +1000

    init.d

--- masterdata/models.py
+++ masterdata/models.py
@@ -5,7 +5,7 @@ class Material(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100, blank=True, null=True)
     regtime = models.DateField(blank=True, null=True)
-    type = models.ForeignKey('masterdata.MatirialType', blank=True, null=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey('masterdata.MaterialType', blank=True, null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey('hr.User', blank=True, null=True, on_delete=models.SET_NULL)
     groups = models.ManyToManyField('hr.Group', blank=True, null=True)
 
