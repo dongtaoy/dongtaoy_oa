d9e86adc39b1154d8ff1181c9c648e06dc147862
dongtaoy <dongt.yu@gmail.com>
2014-08-07 00:01:12 +1000

    init.d

--- system/models.py
+++ system/models.py
@@ -1,7 +1,9 @@
 __author__ = 'dongtaoy'
 from django.db import models
 from django.forms import ModelForm
+from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
+
 
 class Sidebar(models.Model):
     name = models.CharField(max_length=45, blank=True)
@@ -18,8 +20,23 @@ class Label(models.Model):
     name = models.CharField(max_length='50', null=True, blank=True)
     css = models.CharField(max_length='50', null=True, blank=True)
 
+    def __unicode__(self):
+        return self.name
+
+
+class LabelForm(ModelForm):
+    class Meta:
+        model = Label
+        fields = '__all__'
+
 
 class SidebarForm(ModelForm):
     class Meta:
         model = Sidebar
         fields = '__all__'
+
+
+
+
+admin.site.register(Label)
+admin.site.register(Sidebar)
